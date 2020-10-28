554085f488cc13273babac5396e8d9ea7e75768e
Mike Smith <michaesm@marine.rutgers.edu>
2020-10-28 13:29:31 -0400

    modified function: convert_radials_to_netcdf.py
    + function now generates both tabular and multidimensional netcdfs based on user input

--- codar_processing/methods/radials/convert_radials_to_netcdf.py
+++ codar_processing/methods/radials/convert_radials_to_netcdf.py
@@ -10,7 +10,7 @@ import glob
 from codar_processing.src.radials import Radial
 
 
-def main(radial_file, save_path):
+def main(radial_file, save_path, types=['tabular']):
     """
     Main function to parse and qc radial files
     :param radial_file: Path to radial file
@@ -23,18 +23,20 @@ def main(radial_file, save_path):
 
     if r.is_valid():
         try:
-            r.export(
-                os.path.join(save_path, r.file_name.replace('.ruv', '.nc')),
-                'netcdf'
-            )
+            for t in types:
+                r.export(
+                    os.path.join(save_path, t, r.metadata['Site'].split()[0], r.file_name.strip('.ruv')),
+                    'netcdf-{}'.format(t)
+                )
         except ValueError:
             pass
 
 
 if __name__ == '__main__':
-    radial_path = '../../data/radials/SEAB'
+    radial_path = '../../data/radials/ruv/SEAB/'
     radials = glob.glob(os.path.join(radial_path, '*.ruv'))
-    save_path = '../../data/radials_nc/SEAB'
+    save_path = '../../data/radials/nc/'
+    types = ['tabular', 'multidimensional']
 
     for radial in radials:
-        main(radial, save_path)
+        main(radial, save_path, types)
