d6f6a2d88e7b6363b45d02fab3107fc3383f9ef4
Mike Smith <michaesm@marine.rutgers.edu>
2021-12-02 17:31:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,71 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=7.0', 
+    'xarray==0.16.1',
+    'netcdf4==1.5.4',
+    'numpy==1.20',
+    'pyparsing==2.4.7',
+    'scipy',
+    'geopandas',
+    'oceans',
+    'jupyter',
+    'numba',
+    'pytest',
+    'cmocean',
+    'pymongo',
+    'dask',
+    'python-dateutil',
+    'rtree'
+    ]
+
+test_requirements = ['pytest>=3', ]
 
 setup(
-    name='hfradar',
-    version='0.3.5',
-    packages=['hfradar.utilities', 'hfradar', 'hfradar.src', 'hfradar.configs', 'hfradar.methods', 'hfradar.methods.waves', 'hfradar.methods.totals', 'hfradar.methods.radials', 'hfradar.plotting'],
-    url='https://github.com/rucool/hfradarpy',
-    license='MIT',
-    author='mikesmith',
+    author="Michael Smith",
     author_email='michaesm@marine.rutgers.edu',
-    description='Rutgers Center for Ocean Observing Leadership High Frequency Radar Processing toolbox'
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Toolbox to read in High Frequency Radar (HFR) files written in CODAR Tabular Format (CTF).",
+    entry_points={
+        'console_scripts': [
+            'hfradarpy=hfradarpy.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='hfradarpy',
+    name='hfradarpy',
+    packages=find_packages(include=['hfradarpy', 'hfradarpy.*'], exclude="tests"),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/rucool/hfradarpy',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    zip_safe=False,
+    extras_require={
+        "plot": ['cartopy']
+    }
 )
