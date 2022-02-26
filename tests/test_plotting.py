791a0f6d53e620411eb1b663d901f367bf9b0ddf
Mike Smith <michaesm@marine.rutgers.edu>
2022-02-26 01:09:44 -0500

    Update test_plotting.py
    + move imports into each test because ALL tests will fail if the module isn't able to be loaded. this way test_plotting.py will work again when matplotlib, cartopy, cmocean is installed since they are not required packages.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-from hfradarpy.plot.plot_nc import plot_totals as nctotalsplot
-from hfradarpy.plot.plot_nc import plot_radials as ncradialsplot
 
 data_path = (Path(__file__).parent.with_name('hfradarpy') / 'data').resolve()
 output_path = (Path(__file__).parent.with_name('output')).resolve()
@@ -8,6 +6,8 @@ types = ['velocity', 'motion']
 
 
 def test_codar_radials_nc_tabular_plot():
+    from hfradarpy.plot.plot_nc import plot_radials as ncradialsplot
+
     ncfile = data_path / 'radials' / 'nc' / 'tabular' / 'SEAB' / 'RDLi_SEAB_2019_01_01_0200.nc'
     for t in types:
         output_file = output_path / 'plots' / 'tabular' / 'RDLi_SEAB_2019_01_01_0200-{}.png'.format(t)
@@ -15,6 +15,8 @@ def test_codar_radials_nc_tabular_plot():
 
 
 def test_codar_radials_nc_multidimensional_plot():
+    from hfradarpy.plot.plot_nc import plot_radials as ncradialsplot
+
     ncfile = data_path / 'radials' / 'nc' / 'multidimensional' / 'SEAB' / 'RDLi_SEAB_2019_01_01_0200.nc'
     for t in types:
         output_file = output_path / 'plots' / 'multidimensional' / 'RDLi_SEAB_2019_01_01_0200-{}.png'.format(t)
@@ -22,6 +24,8 @@ def test_codar_radials_nc_multidimensional_plot():
 
 
 def test_wera_radials_plot():
+    from hfradarpy.plot.plot_nc import plot_radials as ncradialsplot
+
     ncfile = data_path / 'radials' / 'nc' / 'multidimensional' / 'WERA' / 'RDL_csw_2019_10_24_162300.nc'
     for t in types:
         output_file = output_path / 'plots' / 'wera' / 'RDL_csw_2019_10_24_162300-{}.png'.format(t)
@@ -29,6 +33,7 @@ def test_wera_radials_plot():
 
 
 def test_totals_plot():
+    from hfradarpy.plot.plot_nc import plot_totals as nctotalsplot
     ncfile = data_path / 'totals' / 'oi' / 'nc' / 'hourly' / 'RU_MARA_20190101T000000Z.nc'
     output_file = output_path / 'plots' / 'totals' / 'RU_MARA_20190101T000000Z.png'
     nctotalsplot(ncfile, output_file=output_file, sub=2)
